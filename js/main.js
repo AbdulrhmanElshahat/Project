809a92dc0869f34749182797e36e1148d2e99e40
AbdulrhmanElshahat <https://AbdulrhmanElshahat@github.com>
2022-03-28 23:07:22 +0200

    some additions

--- js/main.js
+++ js/main.js
@@ -4,20 +4,33 @@ let navItems = $('#nav .nav-item')
 let slider = $("#slider");
 let popup = $('.popup');
 let close = $('.close');
-let centersItem = $('.services .nav-item');
-let centers = $('.services .houses .center')
+let sections = [$('.services') , $('.vedio') , $('.gallary')]
+let centerDescription = $('.modal-body p');
+let centerTitle = $('.modal-title');
+let centerAddress = $('.modal-footer .address span');
+let centerPhone = $('.modal-footer .phone-num span');
+let centerBankAcount = $('.modal-footer .acount-num span');
+let centerSocial = $('.modal-footer .social a');
 // Navbar
 
 navItems.click(function(){
     navItems.children('.nav-link').removeClass('active')
     $(this).children('.nav-link').addClass('active')
-
-    // if($(this).children('.nav-link').prop('class') != 'active'){
-    //     console.log('none')
-    // }
 })
 changeNaveStyle(0)
 $(window).on("scroll resize" , _=> changeNaveStyle())
+// sections.forEach(ele =>{
+//     $(window).scroll(function(){
+//         if( $(window).scrollTop() >= ele.offset().top ){
+//             // navItems.children('a').removeClass('active')
+//             getActiveLink = sections.filter(section => section.offset().top >= $(window).scrollTop())
+
+//                 // console.log(navItems.children(a).prop('href' , $(ele).prop('id')))
+//                 // console.log(getActiveLink)
+            
+//         }
+//     })
+// })
 function changeNaveStyle(){
     if($(this).scrollTop() > 0 || $(this).width() <= 575){ shrink()}else{grow()}
 }
@@ -48,13 +61,122 @@ close.click(function(){
     popup.hide(300 , _=>$('body').css('overflow' , 'auto'))
 })
 
-//centers
-centersItem.click(function(){
-    let centerShow = $($(this).data('target'));
-    if(centerShow.css('display') === 'none'){
-        centersItem.removeClass("active")
-        $(this).addClass('active')
-        centers.css('display' , 'none')
-        centerShow.fadeIn(800)
+$.getJSON('../main.json', function(data){
+    let info = data,
+        centersBtns,
+        centersBtnsParent = $('.services .navbar-nav'),
+        mainTitle,
+        mainAddress,
+        mainPhone,
+        mainSocial,
+        mainBank,
+        houseDescription,
+        cardDescription,
+        cardAmountDescription,
+        descriptionsArr;
+    // Creat Centers Btns
+    for(let prop in info){
+        centersBtns = creatNavItem(prop);
+        centersBtnsParent.append(centersBtns)
     }
+    // Centers Btns
+    centersBtns = $(centersBtnsParent.children());    
+    let row = $('.services .row');
+    window.onload = function(){
+        centersBtns.first().click()
+    }
+
+    centersBtns.click(function(){
+        let curentItemeCase = $(this).hasClass('active');
+        if(curentItemeCase === false){
+            toggleClass(centersBtns , 'active' , this)
+            let housesOfCenter = info[$(this).text()];
+            let houseContent;
+            descriptionsArr = []
+            row.children().remove()
+            for(let houseCenter in housesOfCenter){
+                houseDescription = housesOfCenter[houseCenter].description;
+                descriptionsArr.push(houseDescription)
+                cardAmountDescription = houseDescription.split(" " , 14);
+                cardDescription = cardAmountDescription.join(" ");
+                mainTitle = housesOfCenter[houseCenter].title,
+                mainAddress = housesOfCenter[houseCenter].contact.address,
+                mainPhone = housesOfCenter[houseCenter].contact.phonenum,
+                mainSocial = housesOfCenter[houseCenter].contact.facebook,
+                mainBank = housesOfCenter[houseCenter].contact.acountnum;
+                houseContent = creatCard(mainTitle, cardDescription , mainAddress , mainPhone , mainBank , mainSocial)
+                row.append(houseContent)
+            }
+            houseContent = $(row.children())
+            fadeEle(houseContent)
+        }
+        let redMore = $('.red-more')
+        let r = Array.from(redMore)
+        redMore.click(function(){
+            let card = $(this).parents('.card'), 
+                cardTitle = getText(card , ".card-header"),
+                phonNum = getText(card , '.phone-num'),
+                bankAcount = getText(card , '.acount-num'),
+                address = getText(card , '.address'),
+                facebook = card.find('.social').children('a').prop('href');
+                fillEle(centerTitle ,cardTitle )
+                fillEle(centerDescription , descriptionsArr[r.indexOf(this)])
+                fillEle(centerAddress , address)
+                fillEle(centerBankAcount , bankAcount)
+                fillEle(centerPhone , phonNum)
+                centerSocial.prop('href',facebook)
+        })
+    })
+    
 })
+
+function fillEle(ele , tex){
+    ele.html(tex)
+}
+function getText(parentEle , ele){
+   let TheText =  parentEle.find(`${ele}`).text()
+   return TheText
+}
+function toggleClass(ele , className , curentEle){
+    ele.removeClass(className)
+    $(curentEle).addClass(className)
+}
+
+function creatCard(title , description , address , phone , bank , social){
+    return `
+    <div class="col-md-6 col-lg-4 mb-4">
+    <div class="card">
+    <div class="card-header text-center">
+        <h5 class="card-title">${title}</h5>
+    </div> 
+
+    <div class="card-body">
+        <p>
+        ${description} <a href="#" data-bs-toggle="modal" data-bs-target="#myModal" class='red-more text-black-50'>اقرء المزيد</a>
+        </p>
+    </div>
+
+    <div class="card-footer">
+        <div class="phone-num pb-2">
+        <i class="fa-solid fa-mobile-screen-button"></i> <span class="text-black-50">${phone}</span> 
+        </div>
+        <div class="acount-num pb-2">
+        <i class="fa-solid fa-mobile-screen-button"></i> <span class="text-black-50 num">${bank}</span> 
+        </div>
+        <div class="address pb-2">
+        <i class="fa-solid fa-location-dot"></i> <span class="text-black-50">${address}</span>
+        </div>
+        <div class="social ">
+        <a href="${social}"><i class="fa-brands fa-facebook"></i></a>
+        </div>
+    </div>
+    </div>
+</div>`
+}
+function creatNavItem(text){
+    return `<li class="nav-item rounded mb-3 mb-lg-0 ms-lg-3" data-target="#talkha">${text}</li>`
+}
+function fadeEle(ele){
+    ele.css( "display", 'none')
+    ele.fadeIn(500)
+}
